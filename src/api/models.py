3feb3c458d50b8865aabecc30d71ffd4336cbf17
Andrea Muñiz Salvadores <163444471+andreamuniz91@users.noreply.github.com>
2024-07-27 09:39:20 +0000

    clase

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,48 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+            # Do not serialize the password, its a security breach
+        return {"id": self.id,
+                "email": self.email,
+                "is_active": self.is_active,
+                "is_admin": self.is_admin}
+
+
+class Authors(db.model):
+    id = db.Column(db.Integer, primary_Key=True)
+    name = db.column(db.string, unique=False, nullable=False)
+    lastname = db.column(db.string, unique=False, nullable=False)
+    # ForeignKey
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # Relaciones
+    user_to = db.relationship('Users', foreign_keys=['users_id'],
+                               backref=db.backref('author_to', lazy='select'))
+
+    def __repr__(self):
+            return f'<Author: {self.id } - {self.name} - {self.lastname}'
+
+    def serialize(self):
+         return {"id": self.id,
+                "email": self.email,
+                "is_active": self.is_active,
+                "is_admin": self.is_admin}
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_Key=True)
+    title = db.Colum(db.string(100))
+    author_id = db.Column(db.Integer, db.ForeignKey('authos.id'))
+    author_to = db(db.).realationship
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,21 +2,55 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from api.models import db, Users
+
 
 api = Blueprint('api', __name__)
-
-# Allow CORS requests to this API
-CORS(api)
+CORS(api) # Allow CORS requests to this API
 
 
-@api.route('/hello', methods=['POST', 'GET'])
+@api.route('/hello', methods=['GET', 'POST'])
 def handle_hello():
+    response_body = {}
+    response_body['message'] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    return response_body, 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+@api.route('/users', methods=['GET', 'POST'])
+def handle_users():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Users)).scalars()
+        results = [row.serialize() for row in rows]  # List comprehension
+        # Opción 2
+        # results = []
+        # for row in rows:
+        #    results.append(row.serialize())
+        response_body['results'] = results
+        response_body['message'] = "recibí el GET request"
+        return response_body, 200
+    if request.method == 'POST':
+        response_body['message'] = "recibí el POST request"
+        return response_body, 200
+
+
+@api.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_user(user_id):
+    response_body = {}
+    if request.method == 'GET':
+        row = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if not row:
+            response_body['results'] = {}
+            response_body['message'] = f'No existe el usuario {user_id}'
+            return response_body, 404
+        response_body['results'] = row.serialize()
+        response_body['message'] = f'recibí el GET request {user_id}'
+        return response_body, 200
+    if request.method == 'PUT':
+        response_body['message'] = f'recibí el PUT request {user_id}'
+        return response_body, 200
+    if request.method == 'DELETE':
+        response_body['message'] = f'recibí el DELETE request {user_id}'
+        return response_body, 200
